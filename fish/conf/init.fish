c96a5ccf3120c5383737ae03f413fade0254b646
namachan10777 <namachan10777@gmail.com>
2018-01-09 04:31:18 +0900

    add fish config file

---
+++ fish/conf/init.fish
@@ -0,0 +1,18 @@
+set -gx LANG en_US.UTF-8
+set -gx EDITOR vim
+
+switch $TERM
+	case linux
+	case '*'
+		# fcitx setting
+		set -gx XMODIFIERS "@im=fcitx"
+		set -gx QT_IM_MODULE fcitx
+		set -gx GTK_IM_MODULE fcitx
+end
+
+set TMPDIR /tmp/.(whoami)-tmp
+
+if not test -e ~/tmp
+	mkdir -p $TMPDIR
+	ln -s $TMPDIR ~/tmp
+end
