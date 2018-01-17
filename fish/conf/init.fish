d4549ed75c0161bb0f8a9264a6e92289ad86eca3
namachan10777 <namachan10777@gmail.com>
2018-01-17 11:54:31 +0900

    fish: fix OCAMLPARAM

--- fish/conf/init.fish
+++ fish/conf/init.fish
@@ -1,6 +1,6 @@
 set -gx LANG en_US.UTF-8
 set -gx EDITOR vim
-set -gx OCAMLPARAM "_, bin-annot=1"
+set -gx OCAMLPARAM "_,bin-annot=1"
 set -gx OPAMKEEPBUILDDIR 1
 
 set -gx XDG_CONFIG_HOME ~/.config
