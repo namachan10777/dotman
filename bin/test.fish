79f3d7293d752035140e6dbda19ef072bbdf9d9b
namachan10777 <namachan10777@gmail.com>
2019-06-22 12:18:28 +0900

    test.fish return apposite return code

--- bin/test.fish
+++ bin/test.fish
@@ -2,19 +2,24 @@
 
 source (dirname (status -f))/lib.fish
 
+set STATUS 0
+
 function check
 	set SOURCE $argv[1]
 	set TARGET $argv[2]
 	if test -e $TARGET
 		if test ! -L $TARGET
 			echo "✘ "$SOURCE" ... yet linked"
+			set STATUS 1
 		else if test ! (readlink -f $SOURCE) = (readlink $TARGET)
 			echo "✘ "$SOURCE" ... wrong linked"
+			set STATUS 1
 		else
 			echo "✔ "$SOURCE" ... linked" 
 		end
 	else
 		echo "✘ "$SOURCE" ... doesn't exist"
+		set STATUS 1
 	end
 end
 
@@ -24,3 +29,5 @@ end
 
 check $HERE/neovim $XDG_CONFIG_HOME/nvim
 check $HERE/fish $XDG_CONFIG_HOME/fish
+
+exit $STATUS
