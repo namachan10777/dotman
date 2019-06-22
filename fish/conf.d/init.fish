4158831bf589cb07f2331987c1a01a176d8bb0d3
namachan10777 <namachan10777@gmail.com>
2019-06-22 13:31:31 +0900

    opam env when opam exists

--- fish/conf.d/init.fish
+++ fish/conf.d/init.fish
@@ -2,7 +2,9 @@ set -gx LANG en_US.UTF-8
 set -gx EDITOR nvim
 set -gx OCAMLPARAM "_,bin-annot=1"
 set -gx OPAMKEEPBUILDDIR 1
-eval (opam env)
+if type opam > /dev/null 2>&1
+	eval (opam env)
+end
 
 set -gx XDG_CONFIG_HOME ~/.config
 
