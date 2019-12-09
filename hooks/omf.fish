79481238a3cfd7e7e21823b8585391cb4e1f84af
Nakano Masaki <namachan10777@gmail.com>
2019-12-09 16:04:12 +0900

    fix omf install hook

--- hooks/omf.fish
+++ hooks/omf.fish
@@ -1,10 +1,6 @@
 function install
 	echo "Installing omf"
-	if test ! -e ~/.local/share/omf/init.fish
-		if test ! $NONINTERACTIVE; or test $NONINTERACTIVE != 0
-			curl -L https://get.oh-my.fish | fish
-		end
-	end
+	curl -L https://get.oh-my.fish | fish
 end
 
 function check
