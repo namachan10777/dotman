ac8333e379227c9bfd7f4fd08c5a2c2f585d227c
namachan10777 <namachan10777@gmail.com>
2019-12-15 03:53:09 +0900

    impl patch install

--- bin/deploy.py
+++ bin/deploy.py
@@ -2,8 +2,46 @@
 
 import sys
 import json
+import pathlib
+import os.path
+import os
+import shutil
+
+def normalize_path(path_string):
+    return pathlib.Path(os.path.expanduser(os.path.expandvars(path_string))).resolve()
+
+def install_pkg(pkg_path):
+    with open(pkg_path / 'pkg.json', 'r') as f:
+        cfg = json.loads(f.read())
+        dest_cfg = cfg['dest']
+
+        # TODO
+        if 'root' in cfg and cfg['root']:
+            return
+
+        if 'fallback' in cfg:
+            fallback = normalize_path(dest_cfg['fallback'])
+
+        if 'patch' in dest_cfg:
+            print(dest_cfg)
+            for key in dest_cfg['patch'].keys():
+                source = normalize_path(pkg_path / key)
+                dest   = normalize_path(dest_cfg['patch'][key])
+                print(source, dest)
+                if cfg['link']:
+                    os.symlink(source, dest)
+                else:
+                    shutil.copyfile(source, dest)
+            
+
 
 if __name__ == '__main__':
-    with open(sys.argv[1], 'r') as f:
+    cfg_path = normalize_path(sys.argv[1])
+    with open(cfg_path, 'r') as f:
         cfg = json.loads(f.read())
-        print(cfg)
+
+        hooks_path = cfg_path.parent / cfg['hooks']
+        pkgs_path = cfg_path.parent / cfg['pkgs']
+
+        for pkg in pkgs_path.glob('*/'):
+            install_pkg(pkg)
