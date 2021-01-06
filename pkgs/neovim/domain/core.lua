2dfde9af833ace4ae129c1a308001e0b84f262b0
Nakano Masaki <namachan10777@gmail.com>
2021-01-07 00:39:50 +0900

    Fern

--- pkgs/neovim/domain/core.lua
+++ pkgs/neovim/domain/core.lua
@@ -2,7 +2,13 @@ local minpac = require 'domain.minpac'
 
 local load_core = function()
 	minpac:load_repos({
-		{ 'otyn0308/otynium' }
+		{ 'otyn0308/otynium' },
+		{ 'lambdalisue/fern.vim' },
+		{ 'lambdalisue/nerdfont.vim' },
+		{ 'lambdalisue/fern-renderer-nerdfont.vim' },
+		{ 'lambdalisue/fern-git-status.vim' },
+		{ 'lambdalisue/fern-mapping-git.vim' },
+		{ 'lambdalisue/fern-hijack.vim' }
 	})
 	vim.api.nvim_command('colorscheme otynium')
 	vim.api.nvim_command('set tabstop=4')
@@ -11,12 +17,16 @@ local load_core = function()
 	vim.api.nvim_command('set number')
 	vim.api.nvim_command('set relativenumber')
 
-end
 	-- remap
 	vim.api.nvim_set_keymap('n', 'r', 'diwi', { noremap = true })
 	vim.api.nvim_set_keymap('n', 'j', 'gj', { noremap = true })
 	vim.api.nvim_set_keymap('n', 'k', 'gk', { noremap = true })
 	vim.api.nvim_set_keymap('t', '<C-j>', '<C-\\><C-n>', { noremap = true })
 
+	-- fern
+	vim.api.nvim_set_var('fern#renderer', 'nerdfont')
+	vim.api.nvim_set_keymap('n', '<space>f', ':Fern . -drawer<CR>', { noremap = true })
+	vim.api.nvim_set_keymap('x', '<space>f', ':Fern . -drawer<CR>', { noremap = true })
+end
 
 load_core()
