8a3f8f428028629ee5e322ae527ad7b729394778
Nakano Masaki <namachan10777@gmail.com>
2021-01-07 15:44:56 +0900

    neovim: show invisible charcters

--- pkgs/neovim/lua/domain/core.lua
+++ pkgs/neovim/lua/domain/core.lua
@@ -47,6 +47,10 @@ local load_core = function()
 	vim.o.writebackup = false
 	vim.o.cmdheight=2
 	vim.o.updatetime=300
+	vim.o.hls = true
+	vim.wo.list = true
+	vim.o.listchars = 'tab:»-,trail:-,eol:↲,extends:»,precedes:«,nbsp:%'
+	print(vim.o.listchars)
 	if vim.api.nvim_call_function('has', { 'patch-8.1.1564' }) then
 		vim.o.signcolumn = 'number';
 	else
