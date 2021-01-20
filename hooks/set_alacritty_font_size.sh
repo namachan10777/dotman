4b1bfde0f87f895c82f780df108016a84eb8fd17
Nakano Masaki <namachan10777@gmail.com>
2021-01-20 23:35:23 +0900

    change alacritty fontsize on sakanainu

--- hooks/set_alacritty_font_size.sh
+++ hooks/set_alacritty_font_size.sh
@@ -3,7 +3,7 @@
 if [ $(cat /etc/hostname) = "namachan" ]; then
 	sed -ie 's/$ALACRITTY_FONT_SIZE/7.2/' $1
 elif [ $(cat /etc/hostname) = "sakanainu" ]; then
-	sed -ie 's/$ALACRITTY_FONT_SIZE/11/' $1
+	sed -ie 's/$ALACRITTY_FONT_SIZE/12/' $1
 else
 	sed -ie 's/$ALACRITTY_FONT_SIZE/9/' $1
 fi
