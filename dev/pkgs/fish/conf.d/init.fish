f35ce5adfa66895da8db33f7c9912624f15c38e8
Nakano Masaki <nakano.masaki.sj@alumni.tsukuba.ac.jp>
2021-07-16 20:46:39 +0900

    fix ~/tmp script

--- dev/pkgs/fish/conf.d/init.fish
+++ dev/pkgs/fish/conf.d/init.fish
@@ -51,9 +51,9 @@ end
 # ~/tmp {{{
 set TMPDIR /tmp/.(whoami)-tmp
 
-if not test -e $(HOME)/tmp
+if not test -e $HOME/tmp
 	mkdir -p $TMPDIR
-	ln -s $TMPDIR $(HOME)/tmp
+	ln -s $TMPDIR $HOME/tmp
 end
 # }}}
 
