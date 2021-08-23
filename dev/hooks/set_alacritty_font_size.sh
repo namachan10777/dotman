d6cbfb851c3b353d6fd14a2373329112f58c353e
Nakano Masaki <nakano.masaki.sj@alumni.tsukuba.ac.jp>
2021-08-23 15:56:58 +0900

    alacritty font config for Cookpad Macbook

--- dev/hooks/set_alacritty_font_size.sh
+++ dev/hooks/set_alacritty_font_size.sh
@@ -4,6 +4,8 @@ if [ $(cat /etc/hostname) = "ikuraneko" ]; then
 	sed -ie 's/$ALACRITTY_FONT_SIZE/7.2/' $1
 elif [ $(cat /etc/hostname) = "sakanainu" ]; then
 	sed -ie 's/$ALACRITTY_FONT_SIZE/13/' $1
+elif [ $(cat /etc/hostname) = "P1724-19P15U.local" ]; then
+	sed -ie 's/$ALACRITTY_FONT_SIZE/13/' $1
 else
 	sed -ie 's/$ALACRITTY_FONT_SIZE/9/' $1
 fi
