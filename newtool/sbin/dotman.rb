e1ddb5324efecc28414f24caeb3d7b4bc98c30d9
Nakano Masaki <nakano.masaki.sj@alumni.tsukuba.ac.jp>
2021-08-29 02:17:03 +0900

    fix arg name

--- newtool/sbin/dotman.rb
+++ newtool/sbin/dotman.rb
@@ -7,7 +7,7 @@ Version = "0.0.1"
 
 config_file = nil
 opt = OptionParser.new
-opt.on('-c FILE1') {|f| config_file = f}
+opt.on('-c FILE') {|f| config_file = f}
 opt.parse!(ARGV)
 
 if config_file.nil? then
