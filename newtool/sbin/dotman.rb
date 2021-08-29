8f6267f0f59886bd54db723f5c71c4d3c21f5ca6
Nakano Masaki <nakano.masaki.sj@alumni.tsukuba.ac.jp>
2021-08-29 14:32:28 +0900

    read yaml

--- newtool/sbin/dotman.rb
+++ newtool/sbin/dotman.rb
@@ -1,13 +1,33 @@
 #!/usr/bin/ruby
 
 require 'yaml'
+require 'psych'
 require 'optparse'
 
 Version = '0.0.1'.freeze # rubocop:disable Naming/ConstantName
 
-config_file = nil
+config_file_name = nil
 opt = OptionParser.new
-opt.on('-c FILE') { |f| config_file = f }
+opt.on('-c FILE') { |f| config_file_name = f }
 opt.parse!(ARGV)
 
-print(opt.help) if config_file.nil?
+if config_file_name.nil?
+  warn(opt.help)
+  exit!
+end
+
+config = begin
+  config_file = File.open(config_file_name, 'r')
+  YAML.load_file(config_file)
+rescue Psych::SyntaxError => e
+  warn("SyntaxError at #{e.line}:#{e.column}.")
+  config_file.close
+  exit!
+rescue Errno::ENOENT, Errno::EPERM
+  warn("Cannot read file #{config_file_name}")
+  config_file.close
+  exit!
+end
+config_file.close
+
+pp config
