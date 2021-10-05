02d2fe87d245ba5a52bff5f4e48451b29f2bafc8
Masaki <masaki-nakano@cookpad.com>
2021-10-05 23:40:36 +0900

    colorful output

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use clap::{AppSettings, Clap};
 use std::collections::HashMap;
 use std::path::Path;
 use std::process;
+use termion::color;
 #[derive(Clap)]
 #[clap(setting = AppSettings::ColoredHelp)]
 struct Opts {
@@ -27,6 +28,48 @@ struct DryRunOpts {
     config: String,
 }
 
+type Stats = [(String, Vec<(String, Result<bool, dotman::TaskError>)>)];
+
+fn display_result(stats: &Stats) {
+    for (group, tasks) in stats {
+        println!("[{}]", group);
+        for (task_name, result) in tasks {
+            match result {
+                Ok(true) => println!(
+                    "{}[Changed] {}{}",
+                    color::Fg(color::Yellow),
+                    color::Fg(color::White),
+                    task_name
+                ),
+                Ok(false) => println!(
+                    "{}[Ok]      {}{}",
+                    color::Fg(color::Green),
+                    color::Fg(color::LightWhite),
+                    task_name
+                ),
+                Err(dotman::TaskError::WellKnown(msg)) => {
+                    println!(
+                        "{}[Failed]  {}{}",
+                        color::Fg(color::Red),
+                        color::Fg(color::Reset),
+                        task_name
+                    );
+                    println!("  -> {}", msg);
+                }
+                Err(dotman::TaskError::Unknown(e)) => {
+                    println!(
+                        "{}[Failed]  {}{}",
+                        color::Fg(color::Red),
+                        color::Fg(color::Reset),
+                        task_name
+                    );
+                    println!("  -> {}", e);
+                }
+            }
+        }
+    }
+}
+
 fn run(opts: Opts) -> Result<(), dotman::Error> {
     let mut taskbuilders = HashMap::new();
     let cp_builder: dotman::TaskBuilder = Box::new(move |yaml| dotman::tasks::cp::parse(yaml));
@@ -43,7 +86,7 @@ fn run(opts: Opts) -> Result<(), dotman::Error> {
                 dryrun: false,
             };
             let result = playbook.execute_deploy(&ctx)?;
-            println!("{:#?}", result);
+            display_result(&result);
             Ok(())
         }
         Subcommand::DryRun(opts) => {
@@ -53,7 +96,7 @@ fn run(opts: Opts) -> Result<(), dotman::Error> {
                 dryrun: true,
             };
             let result = playbook.execute_deploy(&ctx)?;
-            println!("{:#?}", result);
+            display_result(&result);
             Ok(())
         }
     }
@@ -64,23 +107,47 @@ fn main() {
     match run(opts) {
         Ok(()) => (),
         Err(dotman::Error::AnyScenarioDoesNotMatch) => {
-            eprintln!("any scenario does not match");
+            eprintln!(
+                "{}[Error] {}any scenario does not match",
+                color::Fg(color::Red),
+                color::Fg(color::Reset)
+            );
             process::exit(-1);
         }
         Err(dotman::Error::TaskGroupNotFound(taskgroup_name)) => {
-            eprintln!("taskgroup \"{}\" does not found", taskgroup_name);
+            eprintln!(
+                "{}[Error] {}taskgroup \"{}\" does not found",
+                color::Fg(color::Red),
+                color::Fg(color::Reset),
+                taskgroup_name
+            );
             process::exit(-1);
         }
         Err(dotman::Error::PlaybookLoadFailed(msg)) => {
-            eprintln!("failed to load playbook due to {}", msg);
+            eprintln!(
+                "{}[Error] {}failed to load playbook due to {}",
+                color::Fg(color::Red),
+                color::Fg(color::Reset),
+                msg
+            );
             process::exit(-1);
         }
         Err(dotman::Error::InvalidPlaybook(msg, _)) => {
-            eprintln!("failed to load playbook due to {}", msg);
+            eprintln!(
+                "{}[Error] {}failed to load playbook due to {}",
+                color::Fg(color::Red),
+                color::Fg(color::Reset),
+                msg
+            );
             process::exit(-1);
         }
         Err(dotman::Error::CannotResolveVar(var)) => {
-            eprintln!("cannot resolve var ${}", var);
+            eprintln!(
+                "{}[Error] {}cannot resolve var ${}",
+                color::Fg(color::Red),
+                color::Fg(color::Reset),
+                var
+            );
             process::exit(-1);
         }
     }
