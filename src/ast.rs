85a2ec2b6838439e1e7afde4cd6a38cf4859b4b6
Nakano Masaki <nakano.masaki.sj@alumni.tsukuba.ac.jp>
2021-10-09 14:56:13 +0900

    add ast::not_allowed_member

--- src/ast.rs
+++ src/ast.rs
@@ -84,3 +84,38 @@ impl Value {
         }
     }
 }
+
+pub fn not_allowed_member<'a, K, T>(map: &'a HashMap<K, T>, allowed: &[&K]) -> Vec<(&'a K, &'a T)>
+where
+    K: PartialEq,
+{
+    map.iter()
+        .filter_map(|(key, val)| {
+            if allowed.contains(&key) {
+                None
+            } else {
+                Some((key, val))
+            }
+        })
+        .collect::<Vec<_>>()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use maplit::hashmap;
+
+    #[test]
+    fn test_not_allowed_member() {
+        let map = hashmap! {
+            "a" => 1,
+            "b" => 2,
+            "c" => 3,
+            "d" => 4,
+        };
+        let mut not_allowed = not_allowed_member(&map, &[&"a", &"c"]);
+        not_allowed.sort_by(|(k1, _), (k2, _)| k1.cmp(&k2));
+
+        assert_eq!(not_allowed, vec![(&"b", &2), (&"d", &4)]);
+    }
+}
